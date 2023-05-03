c920bdc24212412152e5cce3025419086ab60e6b
Brady Blair <brady@refactoredmedia.com>
2023-05-03 15:22:29 -0600

    MVP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a-us.storyblok.com', // If using the EU CDN, change this to a.storyblok.com
+        pathname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
